e56866b4a536361fb78cd25e851e486a6c214ec7
Michał M <michalm16@gmail.com>
2016-01-02 16:24:26 +0100

    small fix :)

--- b.php
+++ b.php
@@ -19,7 +19,7 @@
 			'letTheGamesBegin' => 'L6ByjO9I4ruvg00dW1F7zYfYgKePfdroRj1EZjT3i94/d4QazorVHMGDdL5eq3CzixnV9vbhVmWQ+s2ZkEnPGHSKysk4zCcKa6z8zJ/8DX3Q/NzO8Rr9+L4UJxmQLeX6yvb21dbqSAOCjqe+8lcUJgSKIVXZKC1N0QObVnjgof8CmCQFiWwt2zgkSniFd1x8zjZc9IvrUaYOlYciYkyZlPRK0UWF/UM0TBrcD34cb4O2a9IGMGA3gWlKWUA5yB4JZ2XhM5k660yANIhJt4tRxwhenblWB1iO6tl17BVjPDzMfiJIizzQzJSevvtAKlTcty/Hy8RcaxpnDnL+Ty2hzV5ITa1vvQR4ql04hCv4oNBz7x+tzpHPnBkDvsQDGFk3',
 			'theSecond' => 'J4szTPmh+3EyG2w7Zu2jTM8DNg/S0upbvdK8QkP0vzbLWFwwpWpH5497sRRb/K9ZL/aZTyu12obg3ncRvjbiTS6iZg4t2XNOMwITLDJUtXUuEdtGO5olpoacgALyjbKfRWa47ZPzaLpxozCj0Kwxj9mvgejxPa60T3qlWz87PraiuPkKRj4OUaY3q1cYoIq947UdGZbCMHJEdIirYSSMFrwYRVHVkhOwp38q1CJ9PPnrpsy/UXxhARgy3rwAtZZlZZCUvvYTBSJhAYUBvSI7dmVCM+OBeLRnB0iw3ar9LJxix6unq0QRt5xvuqBosyMEWv2oAHL0q8bjJjP+TbkCs0WigoZTkZoBnqXLJ8Y2pX1wS0EiFGPKNVkr00yzHf7K1XJW+OjSjMgWjn0V+qX7xsxCPEdDn7nFvr/+vRuGxamyj5exmfP439+40Topb6XnUITs8TOUCJ4dwEZcVAjPb9O9z3yFQURQXclDgJVQNfI=',
 			'someMath' => 'xxXDRtMt5nls9Me5z+njhIH7RVsYLGVRrk1fChsS5sZ5qYjmGZXeDC8Q31hAfOWzLB70mYDlDFQOVO5axGylc1QqnAI3v3U4QJuopUpn2c6YajTWrW4IXH31ZzyqTdQhHJb61JX7ow5xeLUtlUyG24O8aGG8hZ4Wngeyr0nfdYLIs/nejnKc1Bhva04MFwbejV9cdsUBOv10URpneuLsz89RenG6kizCvMl5S7iJ/SfP+S7RrrftEkMaVn8JPeSeYndi9jEx+TW0S+F5nOw/nLIhij8/R+zPQusEoyPtJi37ggcLGN+zz/+T48MKBcsPiw2RstXMiWpk9UMJOc77LLOjz2Ao731XdeINgrBQsh0d09zuTbT1kWeqfQradsV94kCqroxLhH1ediubiKACiC1J3aHW1uQbhE919m5VF7AqTUsknP40ZuC6XR7c1HCIh1Y4YE7dOnmb7/pocOvEQk9Obxd181+4Ic2U2jPuvwRnq538vW51xzNQsWyyTw9ayLIS+NIyCTFQXaxQKBCa3Io7iYJuU/Wm3steVr2xmMlZUhCctYkID+DFIlxUlnzgOoa6uXK7PSZkMfDXiS1lh1htcFAqkbIDJlOA52EjPXzcAKMmCcuCH4jF8EaHyKBhVCY8kjPzE1a9hvcy8UPC2QRfLuib2fQza7O+XSs/Qem3I8e6N2X41PkgD49LuLewcrpiPDxuK8K29sJtAnYyb1K+d040e9KvXGec1yb6zNMxF1+GM1dm2YOCZMsIiFj4D9cJ5SoIWlG1cr03vUlXEdOdjus46phaA8Q+t2s4ZYe3d/z51Rag4t6UHdxHUY0x8X1AlkFhFHknG5aFLcVGziMYh7r3Ki4TDRhsn3II1u+pKYw0c0hK75uA0h9kZ76NTh2c0z5ci6LPyHrmGA9dBNZZ7b6NiEh6iWYP9PUwbqA=',
-			'endgame' => 'ricGh+d31YNfm0ZtCvRf/gVB/nXldjzlp07k9D0pGXXXF6mtqJdGA3PhclQ9xF0nb7YJ0hO1ktLsJDWHMem4pA=='
+			'endgame' => 'ricGh+d31YNfm0ZtCvRf/gVB/nXldjzlp07k9D0pGXXXF6mtqJdGA3PhclQ9xF0nC9UNYcOn0epazt0IHJFcMw=='
 		];
 		private $hmacKey = 'G39]23<HckvR/5q]'; //http://passwordsgenerator.net/ - don't bother
 		private $input = 'intro';
@@ -63,7 +63,7 @@
 		}
 	
 		function endgame(){
-			echo $this->aesDecrypt($this->input, $this->txtRes['endgame']);
+			echo rtrim($this->aesDecrypt($this->input, $this->txtRes['endgame'])) .  substr($this->input,5);
 		}
 		
 		function nope($checkResult){
